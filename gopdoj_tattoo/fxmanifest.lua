4df6bd77757dff69a63b64442b0092eb4135aff9
LIINight-CPD-singarpore <css920805@gmail.com>
2025-05-13 05:32:50 +0800

    update

---
+++ gopdoj_tattoo/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_tattoo.meta',
+  'stream/tattoo_overlays.xml: PED_OVERLAY_FILE',
+  'stream/shop_tattoo.meta: TATTOO_SHOP_DLC_FILE'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_tattoo.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/tattoo_overlays.xml: PED_OVERLAY_FILE'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'stream/shop_tattoo.meta: TATTOO_SHOP_DLC_FILE'
