96641da61416c6b82489c552b2434b6f2039dc59
LIINight-CPD-singarpore <css920805@gmail.com>
2025-05-13 04:35:25 +0800

    update

---
+++ [gop_n]/gop_eup2/fxmanifest.lua
@@ -0,0 +1,14 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_eup_n2.meta',
+  'mp_f_freemode_01_mp_f_eup_n2.meta',
+  'pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_eup_n2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_eup_n2.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'pedalternatevariations.meta'
